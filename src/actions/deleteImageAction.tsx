0181b166d4a37804b00eca93f34a8751ea481bc8
mahdi <mahdi.azim.elmi@gmail.com>
2024-01-24 19:25:43 +0330

    updated DeleteImageAction to confrom to uploadthing api change

--- src/actions/deleteImageAction.tsx
+++ src/actions/deleteImageAction.tsx
@@ -1,6 +1,6 @@
 "use server";
 
-import { utapi } from "uploadthing/server";
+import { utapi } from "@/server/uploadthing";
 
 // await utapi.deleteFiles("2e0fdb64-9957-4262-8e45-f372ba903ac8_image.jpg");
 // await utapi.deleteFiles([
@@ -8,8 +8,7 @@ import { utapi } from "uploadthing/server";
 //   "1649353b-04ea-48a2-9db7-31de7f562c8d_image2.jpg",
 // ]);
 
-export async function setThemeCookie(imageId: string) {
+export async function deleteImage(imageId: string) {
   console.log(imageId);
   const idk = await utapi.deleteFiles(imageId);
-  console.log(idk);
 }
